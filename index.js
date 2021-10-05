995fc271a154d11bd1ba5d42a05f355ccce446f6
jaebong8 <jaebong90@naver.com>
2021-10-05 11:07:20 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 const mongoose = require('mongoose');
 mongoose
-.connect('mongodb+srv://jaebong8:a13794862!@cluster0.ogold.mongodb.net/todoapp?retryWrites=true&w=majority')
+.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
 .catch((e) => console.log('MongoDB error: ', e));
 
 
 app.get('/', function (req, res) {
-    res.send('Hellow World. 기본페이지입니다.')
+    res.send('Hellow World. 기본페이지입니다. nodemon 설치')
 })
+
+
+
+app.post('/register', (req,res)=> {
+    const user = new User(req.body);
+
+    user.save((err,userInfo)=>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({success: true})
+    })
+
+})
+
+
+
 app.listen(5000, function () {
     console.log('listening on 5000')
 })
