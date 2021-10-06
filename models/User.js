57c075e75ec5aa2944c3e7a15f846ade3f9704df
jaebong8 <jaebong90@naver.com>
2021-10-06 10:31:31 +0900

    auth 추가

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -10,9 +14,9 @@ const userSchema = mongoose.Schema({
         trim: true,
         unique: 1
     },
-    pasword: {
+    password: {
         type: String,
-        maxlength: 50
+        maxlength: 100
     },
     lastname: {
         type: String,
@@ -31,6 +35,60 @@ const userSchema = mongoose.Schema({
     }
 
 })
+
+
+userSchema.pre('save', function(next) {
+    // 비밀번호를 암호화 시킨다
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err);
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next()
+    }
+
+    
+    
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+
+    var user = this;
+
+    // jsonwebtoken 을 이용해서 토큰 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    // user._id + 'scretToken' = token
+
+    user.token = token
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        user.findOne({"_id": decoded,"token": token} ,function (err,user) {
+            if (err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
 
 const User = mongoose.model('User' , userSchema)
 
